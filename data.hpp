b06a46120945b5899081276f47e710aeb22204ba
tim.schubert@tu-bs.de <tim.schubert@tu-bs.de>
2016-11-24 20:16:50 +0100

    Adds enum for status codes.

--- data.hpp
+++ data.hpp
@@ -2,6 +2,7 @@
 #define DATA_HPP
 
 #include <string>
+#include <iostream>
 
 #define DEFAULT_SERVER_PORT "5001"
 
@@ -13,6 +14,8 @@ class address
 
 		address(std::string hostname, std::string portnum);
 };
+
+std::ostream& operator <<(std::ostream& outs, const address &a);
 
 class channel
 {
@@ -28,5 +31,28 @@ class channel
 		const std::string get_name() const;
 		const address get_op();
 };
+
+enum status_code
+{
+	connect_success = 100,
+	connect_error = 101,
+	disconnect_success = 200,
+	disconnect_error = 201,
+	nick_unique = 300,
+	nick_not_authorized = 301,
+	nick_not_unique = 302,
+	join_known_success = 400,
+	join_new_success = 401,
+	not_in_channel = 402,
+	no_such_channel = 403,
+	already_in_channel = 404,
+	leave_successful = 405,
+	client_not_channel_op = 502,
+	msg_delivered = 600,
+	no_such_client = 601,
+	no_such_client_in_channel = 603,
+	privmsg_delivered = 604,
+};
+
 
 #endif /* DATA_HPP */
